df413868f9013e153d6f22b3a3668742844ed9dc
ricky-newsbreak <jiali.huang@newsbreak.com>
2023-07-26 11:14:41 +0800

    update presto auto upgrade script

--- script/download_presto_udf.sh
+++ script/download_presto_udf.sh
@@ -4,6 +4,7 @@
 # This script will try to see if presto service can start after deploy the new udf, is failed, will try to rollback
 # to the original jar or remove the jar if no jar in the beginning.
 
+
 jar_name="udfs-3.0.1-SNAPSHOT.jar"
 download_file_path="/tmp/${jar_name}"
 backup_file_path="/tmp/${jar_name}.bak"
@@ -26,11 +27,35 @@ if [ ! -d "$udf_dir" ]
   echo "Step 1.1: No udf folder found, will create ${udf_dir}"
   sudo mkdir "$udf_dir"
 fi
-echo "Step 2: Copy s3 presto udf jar into ${udf_dir}"
+
+echo "Step 2: Check to see if new jar is the same as old jar."
+if [ -f "$existing_jar" ]
+then
+  origin_md5=`md5sum  $existing_jar`
+  origin_arr=($origin_md5)
+  origin_md5_value=${origin_arr[0]}
+
+  download_file_path="$download_file_path"
+  new_md5=`md5sum $download_file_path`
+  new_arr=($new_md5)
+  new_md5_value=${new_arr[0]}
+
+  if [[ "$origin_md5_value" == "$new_md5_value" ]];then
+    echo "Step 2:skip. mds same."
+    exit 0
+  else
+    echo "Step 2: Found new version of udf, will start update process."
+  fi
+else
+  echo "Step 2: No existing jar found, will start download process."
+fi
+
+echo "Step 3: Copy s3 presto udf jar into ${udf_dir}"
 sudo aws s3 cp "$s3_path" /tmp
 sudo cp "$download_file_path" "$existing_jar"
 
-echo "Step 3: Try to restart presto service..."
+
+echo "Step 4: Try to restart presto service..."
 sudo systemctl restart presto-server.service
 echo "Step 4: Sleep for a while and check system status..."
 sleep 60
@@ -40,13 +65,13 @@ if [ "$system_status" = "active" ]
   echo "Final: Presto service active. Done."
   exit 0
 else
-  echo "Step 4.1: Presto service not healthy. Returned result ${system_status}. Will rollback."
+  echo "Step 5.1: Presto service not healthy. Returned result ${system_status}. Will rollback."
   if [ -f "$backup_file_path" ]
   then
-    echo "Step 4.1.1: Found backup presto udf, will rollback."
+    echo "Step 5.1.1: Found backup presto udf, will rollback."
     sudo cp $backup_file_path $existing_jar
   else
-    echo "Step 4.1.2: No backup file found. Delete current udf and restart."
+    echo "Step 5.1.2: No backup file found. Delete current udf and restart."
     sudo rm $existing_jar
   fi
   sudo systemctl restart presto-server.service
@@ -55,9 +80,7 @@ else
   if [ "$system_status" = "active" ]
     then
     echo "Final: Presto service successfully rolled back. Done."
-    exit 0
   else
     echo "Final: ERROR! Failed to rollback, Presto in unavailable state."
-    exit 1
   fi
 fi
